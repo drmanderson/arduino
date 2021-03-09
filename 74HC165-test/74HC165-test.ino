b34b4954efd0de7be62575dc0b3351f14931bbab
drmanderson <danderson@proofpoint.com>
2021-03-09 19:33:59 +0000

    changed to read 74 internally rather than library

--- 74HC165-test/74HC165-test.ino
+++ 74HC165-test/74HC165-test.ino
@@ -6,8 +6,65 @@ const int dataPin         = 5;  // Connects to the Q7 pin the 165 (9)
 const int clockPin        = 6;  // Connects to the Clock pin the 165 (2)
 const int ploadPin        = 7;  // Connects to Parallel load pin the 165 (1)
 
-byte incoming1;                 // new values for first 74HC165 chip
-byte incoming2;                 // new values for second 74HC165 chip
+#define BYTES_VAL_T unsigned long 
+#define NUMBER_OF_CHIPS 3
+#define DATA_WIDTH NUMBER_OF_CHIPS * 8
+
+BYTES_VAL_T pinValues;                    // new values for  74HC165 chips
+BYTES_VAL_T oldPinValues;                 // old values for  74HC165 chips
+
+BYTES_VAL_T read_values() {
+
+  unsigned long bitVal;
+  BYTES_VAL_T bytesVal = 0;
+  
+  // Write pulse to load pin
+  digitalWrite(clockEnablePin, HIGH);
+  digitalWrite(ploadPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(ploadPin, HIGH);
+  digitalWrite(clockEnablePin, LOW);
+ 
+  // Get data from 74HC165 chips
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, INPUT);
+  for (int i=0; i < DATA_WIDTH; i++) {
+    bitVal = digitalRead(dataPin);
+    /* Set the corresponding bit in bytesVal
+     */
+    bytesVal |= (bitVal << (DATA_WIDTH-1 - i));
+
+    /* Pulse the Clock - rising edge shifts next bit in
+     */
+    digitalWrite(clockPin, HIGH);
+    delayMicroseconds(1);
+    digitalWrite(clockPin, LOW);
+  }
+
+
+  return(bytesVal);
+}
+
+void display_pin_values() {
+  
+  Serial.print("Pin states:\r\n");
+  
+  for (int i=0;i< DATA_WIDTH; i++){
+    Serial.print(" Pin-");
+    Serial.print(i);
+    Serial.print(": ");
+
+    if ((pinValues >> i) & 1)
+      Serial.print("HIGH");
+    else
+      Serial.print("LOW");
+
+    Serial.print("\r\n");
+  }
+
+  Serial.print("\r\n");
+}
+
 
 
 void setup()
@@ -24,38 +81,33 @@ void setup()
   pinMode(dataPin, INPUT);
 
   // Required initial states of these two pins according to the datasheet timing diagram
-  digitalWrite(clockPin, HIGH);
+  digitalWrite(clockPin, LOW);
   digitalWrite(ploadPin,HIGH);
 
+  /*
+   * Read in and display pin states
+   */
+
+  pinValues = read_values();
+  display_pin_values();
+  oldPinValues = pinValues;
 }
 
-void read_values() {
+void loop(){
+  /*
+   * Read in the state of all zones
+   */
 
+   pinValues = read_values();
+
+   /*
+    * If there is a change of state display the changed ones.
+    */
+  if (pinValues != oldPinValues) {
+    Serial.print("*Pin value change detected* \r\n");
+    display_pin_values();
+    oldPinValues = pinValues;
+  }
   
-  // Write pulse to load pin
-  digitalWrite(ploadPin, LOW);
-  delayMicroseconds(5);
-  digitalWrite(ploadPin, HIGH);
-  delayMicroseconds(5);
- 
-  // Get data from 74HC165 chips
-  pinMode(clockPin, OUTPUT);
-  pinMode(dataPin, INPUT);
-  digitalWrite(clockPin, HIGH);
-  digitalWrite(clockEnablePin, LOW);
-  incoming1 = shiftIn(dataPin, clockPin, MSBFIRST);
-  incoming2 = shiftIn(dataPin, clockPin, MSBFIRST); 
-
-  digitalWrite(clockEnablePin, HIGH);
-}
-
-void loop()
-{
-  read_values();
-  Serial.print("Data is: ");
-  Serial.print(incoming1, BIN);
-  Serial.print(" : ");
-  Serial.println(incoming2, BIN);
-
-  delay(500);
+  delay(5);
 }
