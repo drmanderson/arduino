b94727809ade7fca9068c7ef059bee4fd086049a
David Anderson <dave@andersonclan.org.uk>
2022-03-11 23:01:50 +0000

    changed to three button three servo test rig

--- servo/servo.ino
+++ servo/servo.ino
@@ -24,7 +24,7 @@ long lastDebounceTimeC = 0;  // the last time the output pin was toggled
 
 long debounceDelay = 300;    // the debounce time; increase if the output flickers
 
-#define NUMSERVOS  17
+#define NUMSERVOS  3
 
 struct ServoData {
   Adafruit_PWMServoDriver  pwmcard;        // PCA9685 card number
@@ -53,88 +53,19 @@ void setup () {
 
   servo[0].pwmcard = pwm;
   servo[0].pwmcard_socket = 0;
-  servo[0].openangle = 115;
+  servo[0].openangle = 135;
   servo[0].closeangle = 45;
 
-  servo[1].pwmcard = pwm;
-  servo[1].pwmcard_socket = 1;
-  servo[1].openangle = 130;
-  servo[1].closeangle = 60;
+  servo[1].pwmcard = pwm1;
+  servo[1].pwmcard_socket = 0;
+  servo[1].openangle = 135;
+  servo[1].closeangle = 45;
 
-  servo[2].pwmcard = pwm;
-  servo[2].pwmcard_socket = 2;
-  servo[2].openangle = 145;
-  servo[2].closeangle = 50;
+  servo[2].pwmcard = pwm2;
+  servo[2].pwmcard_socket = 0;
+  servo[2].openangle = 135;
+  servo[2].closeangle = 45;
 
-  servo[3].pwmcard = pwm1;
-  servo[3].pwmcard_socket = 1;
-  servo[3].openangle = 70;
-  servo[3].closeangle = 135;
-
-  servo[4].pwmcard = pwm1;
-  servo[4].pwmcard_socket = 2;
-  servo[4].openangle = 70;
-  servo[4].closeangle = 145;
-
-  servo[5].pwmcard = pwm1;
-  servo[5].pwmcard_socket = 7;
-  servo[5].openangle = 120;
-  servo[5].closeangle = 65;
-
-  servo[6].pwmcard = pwm1;
-  servo[6].pwmcard_socket = 8;
-  servo[6].openangle = 120;
-  servo[6].closeangle = 70;
-
-  servo[7].pwmcard = pwm1;
-  servo[7].pwmcard_socket = 9;
-  servo[7].openangle = 70;
-  servo[7].closeangle = 137;
-  
-  servo[8].pwmcard = pwm1;
-  servo[8].pwmcard_socket = 4;
-  servo[8].openangle = 70;
-  servo[8].closeangle = 110;
-
-  servo[9].pwmcard = pwm1;
-  servo[9].pwmcard_socket = 3;
-  servo[9].openangle = 130;
-  servo[9].closeangle = 70;
-
-  servo[10].pwmcard = pwm1;
-  servo[10].pwmcard_socket = 6;
-  servo[10].openangle = 50;
-  servo[10].closeangle = 110;
-
-  servo[11].pwmcard = pwm1;
-  servo[11].pwmcard_socket = 5;
-  servo[11].openangle = 70;
-  servo[11].closeangle = 135;
-
-  servo[12].pwmcard = pwm1;
-  servo[12].pwmcard_socket = 10;
-  servo[12].openangle = 70;
-  servo[12].closeangle = 110;
-
-  servo[13].pwmcard = pwm1;
-  servo[13].pwmcard_socket = 11;
-  servo[13].openangle = 70;
-  servo[13].closeangle = 120;
-
-  servo[14].pwmcard = pwm1;
-  servo[14].pwmcard_socket = 12;
-  servo[14].openangle = 60;
-  servo[14].closeangle = 140;
-  
-  servo[15].pwmcard = pwm1;
-  servo[15].pwmcard_socket = 13;
-  servo[15].openangle = 70;
-  servo[15].closeangle = 140;
-
-  servo[16].pwmcard = pwm2;
-  servo[16].pwmcard_socket = 15;
-  servo[16].closeangle = 50;
-  servo[16].openangle = 120;
 }
 
 int angleToPulse(int ang){
@@ -160,10 +91,10 @@ void loop () {
     // If the buttonA has been pressed lets move the servo
     if  (buttonAState == HIGH)  {
       Serial.println("Button A pressed");
-      Serial.println("Closing all:");
+      Serial.println("Moving to 45 degrees:");
         for (int i = 0; i <= NUMSERVOS-1; i++) {
-          Serial.print("Closing: ");
-          Serial.println(i);
+//          Serial.print("Closing: ");
+//          Serial.println(i);
           move_servo (servo[i].pwmcard, servo[i].pwmcard_socket, servo[i].closeangle);
           delay(500);
         }
@@ -175,10 +106,10 @@ void loop () {
     // If the buttonB has been pressed lets move the servo
     if (buttonBState == HIGH)  {
       Serial.println("Button B pressed");
-      Serial.println("Moving to 170 degrees :");
+      Serial.println("Moving to 135 degrees :");
         for (int i = 0; i <= NUMSERVOS-1; i++) {
-          Serial.print("Opening: ");
-          Serial.println(i);
+ //         Serial.print("Opening: ");
+ //         Serial.println(i);
           move_servo (servo[i].pwmcard, servo[i].pwmcard_socket, servo[i].openangle);
           delay(500);
         }
@@ -192,8 +123,8 @@ void loop () {
       Serial.println("Button C pressed");
       Serial.println("Moving to 90 degrees :");
         for (int i = 0; i <= NUMSERVOS-1; i++) {
-          Serial.print("Centralling: ");
-          Serial.println(i);
+//          Serial.print("Centralling: ");
+//          Serial.println(i);
           move_servo (servo[i].pwmcard, servo[i].pwmcard_socket, 90);
           delay(500);
         }
